502e285cd27821942f684a042270ef531a49dda9
moongni <mooooongni@gmail.com>
2023-06-23 23:49:24 +0900

    Add Naive Bayes Classifier

--- NLP/NaiveBayesClassifier/NavieBayesClassifier.py
+++ NLP/NaiveBayesClassifier/NavieBayesClassifier.py
@@ -7,31 +7,61 @@ Naive Bayes Classifier 구현
     2. 조건부 독립 - 클래스가 주어지면 속성들의 확률은 독립적이다.
 """
 
-
+from typing import Tuple, Set
+from collections import Counter
 import numpy as np
 
 
 class NaiveBayesClassifier:
     def __init__(self):
-        ...
-        
+        self.exception = list(".,?!'()*\"\\:-")
+    
+    def make_vocabulary(self, docs, target) -> Tuple[list, Counter]:
+        voc = set()
+        bigdoc = [Counter()] * self.num_classes
+        for doc, t in zip(docs, target):
+            for e in self.exception:
+                doc = doc.replace(e, "")                
+            words = doc.split(' ')
+            bigdoc[t].update(words)
+            voc = voc | set(words)
+        return list(voc), bigdoc
+                
     def train(self, X, y):
+        assert len(X) == len(y)
+        
         self.train_X = np.array(X)
         self.train_y = np.array(y)
         self.num_docs = self.train_X.shape[0]
         self.classes = np.unique(self.train_y, axis=0)
         self.num_classes = self.classes.shape[0]
-        self.logPrior = np.zeros((self.num_classes,))
+        self.voc, self.bigdoc = self.make_vocabulary(X, y)
+        self.num_words = [
+            sum(c.values())
+            for c in self.bigdoc
+        ]
+        self.log_prior = np.zeros((self.num_classes,))
+        self.log_likelihood = np.zeros((len(self.voc), self.num_classes))
         for cls in self.classes:
-            print((self.train_y == cls).sum())
-            self.logPrior[cls] = np.log((self.train_y == cls).sum() / self.num_docs)
-        ...
+            self.log_prior[cls] = np.log((self.train_y == cls).sum() / self.num_docs + 1)
         
-    def eval(self,):
-        ...
-    
-
-if __name__ == "__main__":
-    nbc = NaiveBayesClassifier()
-    nbc.train([x for x in range(10)], [y for y in range(10)])
-    
+            for i, word in enumerate(self.voc):
+                self.log_likelihood[i, cls] = np.log((self.bigdoc[cls][word] + 1) / (self.num_words[cls] + 1))
+        
+        return self.log_prior, self.log_likelihood, self.voc
+                            
+    def predict(self, docs):
+        preds = []
+        for doc in docs:
+            scores = np.ones((self.num_classes, ))
+            for e in self.exception:
+                doc = doc.replace(e, "")
+            for word in doc.split(' '):
+                if word in self.voc:
+                    word_idx = self.voc.index(word)
+                    for i in range(self.num_classes):
+                        scores[i] *= self.log_likelihood[word_idx, i]
+            scores = scores * self.log_prior
+            preds.append(np.argmax(scores))
+        
+        return np.array(preds)
--- NLP/NaiveBayesClassifier/main.py
+++ NLP/NaiveBayesClassifier/main.py
@@ -2,24 +2,29 @@ import os
 
 import numpy as np
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from NavieBayesClassifier import NaiveBayesClassifier
 
 cur_path = os.path.dirname(os.path.abspath(__file__))
 df = pd.read_csv(os.path.join(cur_path, 'spam.csv'), encoding='latin1')
+df = df[['v1', 'v2']]
 
-labels = np.array(df['v1'])
-docs = np.array(df['v2'])
+encoder = LabelEncoder()
+df['v1'] = encoder.fit_transform(df['v1'])
 
-labels[labels == 'ham'] = 0
-labels[labels == 'spam'] = 1
-labels = labels.astype(np.int64)
+train_set, test_set = train_test_split(df, test_size=0.2)
+print(f"Train set size: {len(train_set)}, Test set size: {len(test_set)}")
 
 nbc = NaiveBayesClassifier()
-nbc.train(docs, labels)
-print(nbc.train_X.shape)
-print(nbc.train_y.shape)
-print(nbc.num_docs)
-print(nbc.num_classes)
-print(nbc.classes)
-print(nbc.logPrior)
+log_prior, log_likelihood, V = nbc.train(train_set['v2'], train_set['v1'])
+
+print(log_prior)
+print(log_likelihood.shape)
+print(len(V))
+
+targets = np.array(test_set['v1'])
+preds = nbc.predict(test_set['v2'])
+accuracy = (targets == preds).sum() / len(targets)
+print(preds)
+print(f"Test Accuracy: {accuracy * 100:.2f}")
